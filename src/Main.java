3e1aa240a381404631dbaf382adbbd57cafae692
alevtina.shalygina <alevtina.shalygina@team.wrike.com>
2024-12-01 18:54:53 +0100

    Hometask 3

--- src/Main.java
+++ src/Main.java
@@ -8,38 +8,6 @@ public class Main {
         int[] numbers = {1, 2, 3, -5, 1, -1, 2};
         System.out.println(Homework3.countUniqueElementsInArray(numbers));
 
-/*        String originalString = "Hello, World!";
-        System.out.println(Homework3.revertString(originalString));*/
-/*
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        System.out.println(Homework3.removeEvenNumbersFromList(numbers));*/
-
-
-/*        String test1 = "([]{})"; // Верно
-        String test2 = "([)]";   // Неверно
-        String test3 = "(([]){})"; // Верно
-        String test4 = "(([]){})}"; // Неверно
-
-        System.out.println(Homework3.checkIfBracketsAreCorrect(test1));
-        System.out.println(Homework3.checkIfBracketsAreCorrect(test2));
-        System.out.println(Homework3.checkIfBracketsAreCorrect(test3));
-        System.out.println(Homework3.checkIfBracketsAreCorrect(test4));*/
-
-/*        Node node1 = new Node("1", null);
-        Node node2 = new Node("2", node1);
-        Node node3 = new Node("3", node2);
-        Node node4 = new Node("4", node3);
-        node1.setNext(node4);
-
-        Node.revertNodeListByHeadNodeExtra(node4);
-
-        System.out.println("AFTER REVERTING NODE LIST: ");
-
-        node1.printInfo();
-        node2.printInfo();
-        node3.printInfo();
-        node4.printInfo();*/
-
     }
 
 
